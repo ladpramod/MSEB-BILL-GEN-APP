7a792b48eca1326b410f3ea7375e1cad35293719
Pramod Lad <jainpramodlad@gmail.com>
2024-05-24 20:21:49 +0530

    Initial commit

---
+++ src/main/java/com/mseb/bill/management/MsebBillGenAppApplication.java
@@ -0,0 +1,13 @@
+package com.mseb.bill.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsebBillGenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsebBillGenAppApplication.class, args);
+	}
+
+}
