bb768f51f41f6b810c2646c52901ba094427a8fa
pramod <jainpramodlad@gmail.com>
2024-05-25 00:32:48 +0530

    implemented controller class
    implemented controller class

--- src/main/java/com/mseb/bill/management/entity/UserEntity.java
+++ src/main/java/com/mseb/bill/management/entity/UserEntity.java
@@ -1,4 +1,25 @@
 package com.mseb.bill.management.entity;
 
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document(collection = "userdata")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
+
+    @MongoId
+    private String userId;
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String mobile;
+    private String email;
+    private String password;
+    private String username;
 }
