a9149a92f79e25939726f18e88c2eb1abf26baff
pramod <jainpramodlad@gmail.com>
2024-05-25 00:47:38 +0530

    Get mapping

--- src/main/java/com/mseb/bill/management/controller/userController.java
+++ src/main/java/com/mseb/bill/management/controller/userController.java
@@ -2,11 +2,13 @@ package com.mseb.bill.management.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
 public class userController {
 
+    @GetMapping("/greet")
     public String hello(){
         return "index";
     }
